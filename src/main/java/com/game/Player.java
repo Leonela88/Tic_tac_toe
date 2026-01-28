c83d5207f0ecf244d0dbb7d88187fd829a2913cd
Leonela88 <rivasaela@gmail.com>
2026-01-28 19:46:35 +0100

    feat:create player obj

--- src/main/java/com/game/Player.java
+++ src/main/java/com/game/Player.java
@@ -1,5 +1,21 @@
 package com.game;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Player {
     
+    private char symbol;
+    private Scanner scanner;
+    private Random random = new Random();
+
+    public Player(char symbol, Scanner scanner) {
+        this.symbol = symbol;
+        this.scanner = scanner;
+    }
+
+    
+    public Player(char symbol) {
+        this.symbol = symbol;
+    }
 }
