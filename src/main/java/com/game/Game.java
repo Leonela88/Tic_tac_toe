98a8c093e42fc1c60669a0b68363a9c1db2d8a89
Jen Ceballos <jenniferceballoscastro@gmail.com>
2026-01-28 19:30:48 +0100

    feat:create game object

--- src/main/java/com/game/Game.java
+++ src/main/java/com/game/Game.java
@@ -1,6 +1,17 @@
 package com.game;
 
+import java.util.Scanner;
 
 public class Game {
+    private Board board;
+    private Player player;
+    private Player computer;
+    private Scanner scanner;
 
+    public Game() {
+        board = new Board();
+        scanner = new Scanner(System.in);
+        player = new Player('X', scanner);
+        computer = new Player('O');
+    }
 }
