0e7d2af347e694c9d957e56c79b859cc6ebcfdf4
Leonela88 <rivasaela@gmail.com>
2026-01-28 18:55:14 +0100

    feat:function board position

--- src/main/java/com/game/Board.java
+++ src/main/java/com/game/Board.java
@@ -13,3 +13,16 @@ public class Board {
         System.out.println("-+-+-");
         System.out.println(board[2][0] + "|" + board[2][1] + "|" + board[2][2]);
     }
+
+       public boolean isValidMove(int pos) { 
+        int row = (pos - 1) / 3;
+        int col = (pos - 1) % 3;
+        return board[row][col] == ' ';
+    }
+
+    public void placeMove(int pos, char symbol) {
+        int row = (pos - 1) / 3;
+        int col = (pos - 1) % 3;
+        board[row][col] = symbol;
+    }
+}
