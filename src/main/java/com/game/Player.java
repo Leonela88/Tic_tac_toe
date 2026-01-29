e2ad6d679842b33e1e6bde39ff872c39fb220b61
Gracielagq07 <gracielagq07@gmail.com>
2026-01-29 15:24:31 +0100

    update test

--- src/main/java/com/game/Player.java
+++ src/main/java/com/game/Player.java
@@ -18,22 +18,36 @@ public class Player {
         this.symbol = symbol;
     }
 
-    public void play(Board board) { 
+    public void play(Board board) {
         int move;
-        while (true){
-          System.out.println("Elige posicion (1-9):");  
-          move = Integer.parseInt(scanner.nextLine());
-          if (board.isValidMove(move)) break;
-          System.out.println("Casilla ocupada");
+
+        while (true) {
+            System.out.println("Elige posición (1-9):");
+            move = Integer.parseInt(scanner.nextLine());
+
+            if (move < 1 || move > 9) {
+                System.out.println("Debes elegir un número del 1 al 9");
+                continue;
+            }
+
+            if (!board.isValidMove(move)) {
+                System.out.println("Casilla ocupada");
+                continue;
+            }
+
+            break;
         }
+
         board.placeMove(move, symbol);
-        }
+    }
 
     public void playRandom(Board board) {
         int move;
+
         do {
             move = random.nextInt(9) + 1;
         } while (!board.isValidMove(move));
+
         System.out.println("Eleccion computadora " + move);
         board.placeMove(move, symbol);
     }
