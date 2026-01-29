06f796d07558e4867ef16e374c60c6203e774b40
majoz-t <majozta@gmail.com>
2026-01-29 14:29:37 +0100

    feat: instruction game

--- src/main/java/com/game/Game.java
+++ src/main/java/com/game/Game.java
@@ -15,9 +15,15 @@ public class Game {
         computer = new Player('O');
     }
 
+    public void instructionsGame(){
+        System.out.println("¡Bienvenido a Tic-Tac-Toe!");
+        System.out.println("¿Preparado para comenzar?");
+        System.out.println("Jugarás con la X y como primer paso tendrás que elegir la posición en la que jugarás, desde la posición 1 a la 9");
+        System.out.println("Ganarás si logras tener tres cruces consecutivas en horizontal, vertical o diagonal");
+    }
+
     public void start() {
         board.print();
-
         while (true) {
             player.play(board);
             if (board.isGameFinished('X'))
