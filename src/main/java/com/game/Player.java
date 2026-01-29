d4df05cba20ad2ce13d0195d53c77c1f62e0c380
Leonela88 <rivasaela@gmail.com>
2026-01-29 19:51:37 +0100

    fixed: allowed board range numbers

--- src/main/java/com/game/Player.java
+++ src/main/java/com/game/Player.java
@@ -18,16 +18,29 @@ public class Player {
         this.symbol = symbol;
     }
 
-    public void play(Board board) { 
+    public void play(Board board) {
         int move;
-        while (true){
-          System.out.println("Elige posicion (1-9):");  
-          move = Integer.parseInt(scanner.nextLine());
-          if (board.isValidMove(move)) break;
-          System.out.println("Casilla ocupada");
+
+        while (true) {
+            System.out.println("Elige posición (1-9):");
+            move = Integer.parseInt(scanner.nextLine());
+
+            if (move < 1 || move > 9) {
+                System.out.println("Debes elegir un número del 1 al 9");
+                continue;
+            }
+
+            if (!board.isValidMove(move)) {
+                System.out.println("Casilla ocupada");
+                continue;
+            }
+
+            break;
+      
         }
+
         board.placeMove(move, symbol);
-        }
+    }
 
     public void playRandom(Board board) {
         int move;
